21bfc1d36f1ecd32dae0e660545f581ae7e41b83
Leonardo Massaroli <leonardo.massaroli@accesspay.com>
2022-09-20 13:59:46 +0100

    Projects refactor

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,22 @@ import React, { useState } from "react";
 import "./App.css";
 import "font-awesome/css/font-awesome.min.css";
 
+const projects = [
+  {
+    title: "Pixi Shooter",
+    imgUrl: "/portfolio/pixi-shooter.png",
+    url: "https://leoflood.github.io/pixi-shooter",
+    description:
+      "A demo shooter developed with Typescript, Pixi JS and React, uses the composite design pattern.",
+  },
+  {
+    title: "Ezejedrez",
+    imgUrl: "/portfolio/ezejedrez.png",
+    url: "https://play.google.com/store/apps/details?id=com.gamesflood.ezejedrez",
+    description: "A chess developed on React Native using Typescript and TDD.",
+  },
+];
+
 export default function App() {
   const [selectedLink, setSelectedLink] = useState("");
 
@@ -53,28 +69,14 @@ export default function App() {
           </header>
 
           <ul className="projects">
-            <li>
-              <a
-                href="https://leoflood.github.io/pixi-shooter"
-                rel="me"
-                target="_blank"
-                className="project"
-              >
-                <img src="/portfolio/pixi-shooter.png" />
-                <b>Pixi Shooter</b>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://play.google.com/store/apps/details?id=com.gamesflood.ezejedrez"
-                rel="me"
-                target="_blank"
-                className="project"
-              >
-                <img src="/portfolio/ezejedrez.png" />
-                <b>Ezejedrez</b>
-              </a>
-            </li>
+            {projects.map((p) => (
+              <li key={p.title}>
+                <a href={p.url} rel="me" target="_blank" className="project">
+                  <img src={p.imgUrl} />
+                  <b>{p.title}</b>
+                </a>
+              </li>
+            ))}
           </ul>
         </section>
 
