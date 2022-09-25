1b6842d7d2594dd354967f451b5b2caa3d645c97
Leonardo Massaroli <leoflood98@gmail.com>
2022-09-25 13:00:48 +0100

    Adding independenciajuego project

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import "font-awesome/css/font-awesome.min.css";
-
-const projects = [
-  {
-    title: "Pixi Shooter",
-    imgUrl: "/portfolio/pixi-shooter.png",
-    url: "https://leoflood.github.io/pixi-shooter",
-    description:
-      "A demo shooter developed with Typescript, Pixi JS and React, uses the composite design pattern.",
-  },
-  {
-    title: "Ezejedrez",
-    imgUrl: "/portfolio/ezejedrez.png",
-    url: "https://play.google.com/store/apps/details?id=com.gamesflood.ezejedrez",
-    description: "A chess developed on React Native using Typescript and TDD.",
-  },
-];
+import projects from "./projects";
 
 export default function App() {
   const [selectedLink, setSelectedLink] = useState("");
