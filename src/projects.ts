d30c8b5a73230c997a2ea5a9cc9c876c6866af5a
Leonardo Massaroli <leoflood98@gmail.com>
2023-03-16 17:58:37 -0300

    Project Pixi shooter put first, style improved

--- src/projects.ts
+++ src/projects.ts
@@ -1,20 +1,20 @@
 const projects = [
   {
-    title: "Scissors War",
-    imgUrl: "/portfolio/scissors-war.png",
-    url: "https://leoflood.github.io/scissors-war/",
-    codeUrl: "https://github.com/leoflood/scissors-war",
-    description:
-      "Based on the classic game of Rock Paper Scissors in this game you can play as one or two players.",
-    technologies: ["React", "Pixi.js"],
-  },
-  {
     title: "Pixi Shooter",
     imgUrl: "/portfolio/pixi-shooter.png",
     url: "https://leoflood.github.io/pixi-shooter",
     codeUrl: "https://github.com/leoflood/pixi-shooter",
     description:
       "A demo shooter developed with Typescript, Pixi JS and React, uses the composite design pattern.",
+    technologies: ["React", "Pixi.js"],
+  },
+  {
+    title: "Scissors War",
+    imgUrl: "/portfolio/scissors-war.png",
+    url: "https://leoflood.github.io/scissors-war/",
+    codeUrl: "https://github.com/leoflood/scissors-war",
+    description:
+      "Based on the classic game of Rock Paper Scissors in this game you can play as one or two players.",
     technologies: ["React", "Pixi.js"],
   },
   {
