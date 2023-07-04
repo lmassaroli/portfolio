dd4bbba9779e3fe3120d30dfb2f4a1e34366f460
Leonardo Massaroli <leoflood98@gmail.com>
2023-07-04 09:34:27 -0300

    Projects updated

--- src/projects.ts
+++ src/projects.ts
@@ -9,18 +9,9 @@ const projects = [
     technologies: ["React", "Pixi.js"],
   },
   {
-    title: "Scissors War",
-    imgUrl: "/portfolio/scissors-war.png",
-    url: "https://leoflood.github.io/scissors-war/",
-    codeUrl: "https://github.com/leoflood/scissors-war",
-    description:
-      "Based on the classic game of Rock Paper Scissors in this game you can play as one or two players.",
-    technologies: ["React", "Pixi.js"],
-  },
-  {
     title: "Ezejedrez",
     imgUrl: "/portfolio/ezejedrez.png",
-    codeUrl: "https://github.com/leoflood/ezejedrez",
+    codeUrl: "https://github.com/sophie-games/ezejedrez",
     description: "A chess developed on React Native using Typescript and TDD.",
     technologies: ["React Native"],
   },
@@ -31,6 +22,15 @@ const projects = [
     description:
       "Turn based 3D strategy game about The Independence of South America.",
     technologies: ["Node.js", "React", "Babylon.js"],
+  },
+  {
+    title: "Scissors War",
+    imgUrl: "/portfolio/scissors-war.png",
+    url: "https://leoflood.github.io/scissors-war/",
+    codeUrl: "https://github.com/leoflood/scissors-war",
+    description:
+      "Based on the classic game of Rock Paper Scissors in this game you can play as one or two players.",
+    technologies: ["React", "Pixi.js"],
   },
 ];
 
